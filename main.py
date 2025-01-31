6969add2514d9eaf6357c2b3f413c9cb248eec54
Dietmar Puschmann <depuschm@gmx.de>
2025-01-31 18:59:30 +0100

    Added report (solution uses filter)

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         word_count = count_words(file_contents)
-        character_count = count_characters(file_contents)
-        print(character_count)
+        char_count = count_characters(file_contents)
+        print_report(path_to_file, word_count, char_count)
 
 def count_words(text):
     words = text.split()
@@ -11,12 +11,27 @@ def count_words(text):
 
 def count_characters(text):
     result = {}
-    for character in text:
-        character_lower = character.lower()
-        if character_lower not in result:
-            result[character_lower] = 1
+    for char in text:
+        char_lower = char.lower()
+        if char_lower not in result:
+            result[char_lower] = 1
         else:
-            result[character_lower] = result[character_lower] + 1
+            result[char_lower] += 1
     return result
+
+def sort_on(dict):
+    return dict["num"]
+
+def print_report(path_to_file, word_count, character_count):
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document\n")
+
+    is_alpha = lambda x : x.isalpha()
+    characters_alphabet = filter(is_alpha, character_count)
+    for char in characters_alphabet:
+        print(f"The '{char}' character was found {character_count[char]} times")
+
+    print("--- End report ---")
+
 
 main("books/frankenstein.txt")
