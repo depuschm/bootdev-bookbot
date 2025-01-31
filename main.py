e4f4ce01a7f85a0a185f3b75269909dde4420ec9
Dietmar Puschmann <depuschm@gmx.de>
2025-01-31 18:40:37 +0100

    Added character count

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 def main(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
+        word_count = count_words(file_contents)
+        character_count = count_characters(file_contents)
+        print(character_count)
 
-if __name__ == "__main__":
-    main("books/frankenstein.txt")
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def count_characters(text):
+    result = {}
+    for character in text:
+        character_lower = character.lower()
+        if character_lower not in result:
+            result[character_lower] = 1
+        else:
+            result[character_lower] = result[character_lower] + 1
+    return result
+
+main("books/frankenstein.txt")
