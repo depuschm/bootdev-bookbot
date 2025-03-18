d33a86da37474d3a5625c7b311c9dc93d46699ff
Dietmar Puschmann <depuschm@gmx.de>
2025-03-19 00:53:51 +0100

    Book path can be passed as argument to script now

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import sys
 from stats import count_words, count_characters, print_report
 
-def main(path_to_file):
+def main():
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_file = sys.argv[1]
+
     with open(path_to_file) as f:
         file_contents = f.read()
         word_count = count_words(file_contents)
         char_count = count_characters(file_contents)
         print_report(path_to_file, word_count, char_count)
 
-main("books/frankenstein.txt")
+if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+main()
