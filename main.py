cb730cf5657e50a331105c354e1b52b6bfe17236
Dietmar Puschmann <depuschm@gmx.de>
2025-03-19 00:19:50 +0100

    Updated report text

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, print_report
 
 def main(path_to_file):
     with open(path_to_file) as f:
@@ -6,23 +6,5 @@ def main(path_to_file):
         word_count = count_words(file_contents)
         char_count = count_characters(file_contents)
         print_report(path_to_file, word_count, char_count)
-
-def sort_on(d):
-    return d[1]
-
-def print_report(path_to_file, word_count, character_count):
-    print(f"--- Begin report of {path_to_file} ---")
-    print(f"{word_count} words found in the document\n")
-
-    is_alpha = lambda x : x.isalpha()
-    characters_alphabet = {
-        k:v for (k,v) in sorted(character_count.items(), key=sort_on, reverse=True)
-        if is_alpha(k)
-    }
-    for char in characters_alphabet:
-        print(f"The '{char}' character was found {character_count[char]} times")
-
-    print("--- End report ---")
-
 
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -11,3 +11,23 @@ def count_characters(text):
         else:
             result[char_lower] += 1
     return result
+
+def sort_on(d):
+    return d[1]
+
+def print_report(path_to_file, word_count, character_count):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    is_alpha = lambda x : x.isalpha()
+    characters_alphabet = {
+        k:v for (k,v) in sorted(character_count.items(), key=sort_on, reverse=True)
+        if is_alpha(k)
+    }
+    for char in characters_alphabet:
+        print(f"{char}: {character_count[char]}")
+
+    print("============= END ===============")
